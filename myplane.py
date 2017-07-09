7bac79dd137d72652d9ad5bb31bd76c27a26a133
hiric <956815476@qq.com>
2017-07-09 22:03:45 +0800

    定义我方飞机

---
+++ myplane.py
@@ -0,0 +1,43 @@
+import pygame
+
+class myPlane(pygame.sprite.Sprite):
+    def __init__(self,bg_size):
+        pygame.sprite.Sprite.__init__(self)
+        self.image1 = pygame.image.load("./images/me1.png").convert_alpha()
+        self.image2 = pygame.image.load("./images/me2.png").convert_alpha()
+        #获得飞机大小的矩形区域
+        self.rect = self.image1.get_rect()
+        self.width, self.height = bg_size[0],bg_size[1]
+        #出去底部状态栏后矩形的大小
+        self.rect.left, self.rect.top = \
+                        (self.width - self.rect.width) // 2,\
+                        self.height - self.rect.height - 60
+        
+        self.speed = 10
+        
+    #设置飞机的移动，若超出边界则重新定义位置 
+    def moveUp(self):
+        if self.rect.top > 0:
+            self.rect.top -= self.speed
+        else:
+            self.rect.top = 0
+
+    def moveDown(self):
+        if self.rect.bottom < self.height - 60:
+            self.rect.top += self.speed
+        else:
+            self.rect.bottom = self.height - 60
+
+    def moveLeft(self):
+        if self.rect.left > 0:
+            self.rect.left -= self.speed
+        else:
+            self.rect.left = 0
+
+    def moveRight(self):
+        if self.rect.right < self.width:
+            self.rect.left += self.speed
+        else:
+            self.rect.right = self.width
+
+            
