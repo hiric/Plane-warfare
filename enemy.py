086026debb36ad9922152db75290b432da97a53c
hiric <956815476@qq.com>
2017-07-11 18:31:56 +0800

    update
    绘制血槽，添加爆炸效果。

--- enemy.py
+++ enemy.py
@@ -37,10 +37,13 @@ class SmallEnemy(pygame.sprite.Sprite):
                         randint(-5 * self.height,0)
 
 class MidEnemy(pygame.sprite.Sprite):
+    energy = 8
+    
     def __init__(self,bg_size):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load("./images/enemy2.png").convert_alpha()
+        self.image_hit = pygame.image.load("./images/enemy2.png").convert_alpha()
         self.destroy_images = []
         self.destroy_images.extend([\
             pygame.image.load("./images/enemy2_down1.png").convert_alpha(),\
@@ -56,6 +59,8 @@ class MidEnemy(pygame.sprite.Sprite):
                         randint(0,self.width - self.rect.width),\
                         randint(-10 * self.height,-self.height)
         self.mask = pygame.mask.from_surface(self.image)
+        self.energy = MidEnemy.energy
+        self.hit = False
         
     def move(self):
         if self.rect.top < self.height:
@@ -65,16 +70,20 @@ class MidEnemy(pygame.sprite.Sprite):
 
     def reset(self):
         self.active = True
+        self.energy = MidEnemy.energy
         self.rect.left, self.rect.top = \
                         randint(0,self.width - self.rect.width),\
                         randint(-10 * self.height,-self.height)
 
 class BigEnemy(pygame.sprite.Sprite):
+    energy = 20
+    
     def __init__(self,bg_size):
         pygame.sprite.Sprite.__init__(self)
 
         self.image1 = pygame.image.load("./images/enemy3_n1.png").convert_alpha()
         self.image2 = pygame.image.load("./images/enemy3_n2.png").convert_alpha()
+        self.image_hit = pygame.image.load("./images/enemy3_hit.png").convert_alpha()
         self.destroy_images = []
         self.destroy_images.extend([\
             pygame.image.load("./images/enemy3_down1.png").convert_alpha(),\
@@ -92,6 +101,8 @@ class BigEnemy(pygame.sprite.Sprite):
                         randint(0,self.width - self.rect.width),\
                         randint(-15 * self.height,-5 * self.height)
         self.mask = pygame.mask.from_surface(self.image1)
+        self.energy = BigEnemy.energy
+        self.hit = False
         
     def move(self):
         if self.rect.top < self.height:
@@ -101,6 +112,7 @@ class BigEnemy(pygame.sprite.Sprite):
 
     def reset(self):
         self.active = True
+        self.energy = BigEnemy.energy
         self.rect.left, self.rect.top = \
                         randint(0,self.width - self.rect.width),\
                         randint(-10 * self.height,-5 * self.height)
